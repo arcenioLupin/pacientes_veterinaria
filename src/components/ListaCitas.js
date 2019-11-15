9faee2cb7869ab0656ff8bedcba9f09814a03c5c
arcenioLupin <arceniovilca.caceres@gmail.com>
2019-11-14 21:47:35 -0500

    termino de proyecto de citas veterinaria- publicando en netlify

--- src/components/ListaCitas.js
+++ src/components/ListaCitas.js
@@ -1,27 +1,38 @@
 
 import React from 'react';
 import Cita from './Cita';
+import PropTypes from 'prop-types';
 
-const ListaCitas = ({citas,eliminarCita}) => (      
-  <div className = "card mt-2 py-5">
-    <div className="card-body">
-       <h2 className ="card-title text-center">Administra las citas aquí</h2>
+const ListaCitas = ({citas,eliminarCita}) => {    
+    // Obteniendo mensaje a partir del tamaño de las citas
 
-       <div className = "lista-citas">
-           {citas.map(cita=>(
-              <Cita 
-                key = {cita.id}
-                cita = {cita}
-                eliminarCita = {eliminarCita}
-              />
-           ))}
-
-       </div>
-
+    const mensaje = Object.keys(citas).length === 0 ? 'No hay citas': 'Administra las citas aquí';
+  
+    return(
+      <div className = "card mt-2 py-5">
+      <div className="card-body">
+         <h2 className ="card-title text-center">{mensaje}</h2>
+  
+         <div className = "lista-citas">
+             {citas.map(cita=>(
+                <Cita 
+                  key = {cita.id}
+                  cita = {cita}
+                  eliminarCita = {eliminarCita}
+                />
+             ))}
+  
+         </div>
+  
+      </div>
+  
     </div>
 
-  </div>
-);
-
+    )
+  };
+ListaCitas.propTypes ={
+  citas: PropTypes.array.isRequired,
+  eliminarCita : PropTypes.func.isRequired
+}
 
 export default ListaCitas;
